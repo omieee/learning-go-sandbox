cfddef2c1d6a8801fe64bdd333059629170e3306
Om Shanker <omnewi@gmail.com>
2026-07-18 09:55:04 +0530

    Added week4 struct example

---
+++ month-1/week-4/student-report/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Student struct {
+	Name       string `json:"name"`
+	Class      string `json:"class"`
+	RollNumber int    `json:"rollnumber"`
+	Adderess
+}
+
+type Adderess struct {
+	AddressLineOne string `json:"addLineOne"`
+	Pincode        int    `json:"piccode"`
+}
+
+func (stud *Student) UpdateProUpdateStudentDetail() {
+	stud.Name = "John Doe"
+	stud.Class = "Class 4"
+	stud.RollNumber = 23
+	stud.AddressLineOne = "12 Main 14 Cross"
+	stud.Pincode = 100102
+}
+
+func main() {
+	stu := Student{}
+	stu.UpdateProUpdateStudentDetail()
+	fmt.Println("Student Data: ", stu)
+}
